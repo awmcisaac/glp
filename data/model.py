36ae17ee227fdc4ce97bbd2789c85a802f30ab71
rossana24 <rossana.cervesato@hotmail.it>
2023-01-28 19:37:46 +0100

    Low features extraction

--- data/model.py
+++ data/model.py
@@ -1,16 +1,6 @@
 import torch
-import torchvision
-from torchvision import transforms as T # functional utilities
-import torch.nn.functional as F
-from torchvision.models.feature_extraction import create_feature_extractor
-
-
 import torchvision.models as models
-
-#### MODEL
-# Composition module (2-layer MLP)
-# ResNet50 backbone
-# Final model (ResNet50 + Composition + Classifier)
+from typing import Callable
 
 class Model(torch.nn.Module):
 
@@ -35,35 +25,40 @@ class Model(torch.nn.Module):
         )
 
         # classifier
-        # dim = 28*28
         self.classifier = torch.nn.Linear(2048, 620)
 
+        # Feature extraction
+        self.layers = ["layer2", "layer3"]
+        self._features = {layer: torch.empty(0) for layer in self.layers}
 
+        for layer_id in self.layers:
+            layer = dict([*self.model_resnet.named_modules()])[layer_id]
+            layer.register_forward_hook(self.save_outputs_hook(layer_id))
+
+    def save_outputs_hook(self, layer_id: str) -> Callable:
+        def fn(_, __, output):
+            self._features[layer_id] = output
+
+        return fn
 
     def forward(self, img, word_emb):
-
-        img = self.model_resnet(img)
-        word_emb = self.f_gate(word_emb)
-
-        # channel like product of fgate anf fimg -> keepind dim of x1, it supports backprop
-        # torch.Size([1, 2048, 1, 2048])
-        combination = torch.mul(img, word_emb.unsqueeze(dim=-1).unsqueeze(dim=-1))
-
         # low level feature extraction
         # use the output feature maps from ResNet block 2 and 3 as low-level features.
         # layer2.3.relu_2 -> torch.Size([1, 512, 28, 28])
         # layer3.5.relu_2 -> torch.Size([1, 1024, 14, 14])
+        # Low feature = dict_keys(['layer2', 'layer3'])
 
-        return_nodes = {
-            "layer2.3.relu_2": "layer2",
-            "layer3.5.relu_2": "layer3"
-        }
-        model2 = create_feature_extractor(img, return_nodes=return_nodes)
-        intermediate_outputs = model2(torch.rand(1, 3, 224, 224))
-        low_features = torch.cat(intermediate_outputs['layer2'], intermediate_outputs['layer3']) # list of 2 tensors
+        _ = self.model_resnet(img)
+
+        img = self.model_resnet(img)
+        word_emb = self.f_gate(word_emb)
+
+        # channel like product of fgate anf fimg
+        combination = torch.mul(img, word_emb.unsqueeze(dim=-1).unsqueeze(dim=-1))  # torch.Size([1, 2048, 1, 2048])
 
         # concatenation of penultimate layer of Resnet and result of the compound module
-        concatenation = torch.cat((low_features, combination), dim=0)
+        #### TO DO Check torch.cat()
+        concatenation = torch.cat((self._features['layer2'], self._features['layer3'], combination))
 
         output = torch.sigmoid(self.classification(concatenation))
 
@@ -71,3 +66,4 @@ class Model(torch.nn.Module):
 
 
 if __name__ == "__main__":
+    print()
