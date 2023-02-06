3fba92857a16c08b0c1ee3570d3f74be951687f3
Andrew McIsaac <andrew.mcisaac@ntlworld.com>
2023-02-06 13:52:23 +0000

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models.feature_extraction import create_feature_extractor
 from typing import Callable
 
 class Model(torch.nn.Module):
@@ -15,7 +16,7 @@ class Model(torch.nn.Module):
 
         # ResNet backbone
         self.model_resnet = resnet50(weights=ResNet50_Weights.DEFAULT)  # Initializing ResNet50 by pytorch
-        self.model_resnet.fc = torch.nn.Identity()  # Replace the fully connected layer with an identity
+#        self.model_resnet.fc = torch.nn.Identity()  # Replace the fully connected layer with an identity
 
         # gating mechanism
         self.f_gate = torch.nn.Sequential(
@@ -25,53 +26,38 @@ class Model(torch.nn.Module):
             torch.nn.Sigmoid(),
         )
 
-        # classifier
-        self.classifier = torch.nn.Linear(4096, 620)
+        # classifier - composition 2048 + layer2 512 + layer3 1024 = 3584
+        self.classifier = torch.nn.Linear(3584, 620)
 
         # Feature extraction
-        self.layers = ["layer2", "layer3"]
-        self._features = {layer: torch.empty(0) for layer in self.layers}
+        # self.layers = ["layer2", "layer3"]
+        # self._features = {layer: torch.empty(0) for layer in self.layers}
+
+        # use the output feature maps from ResNet block 2 and 3 as low-level features.
+        # layer4 is output before final fully connected layer
+        self.return_nodes = {
+            "layer2": "layer2",
+            "layer3": "layer3",
+            "layer4": "layer4"
+        }
+        self.model_resnet = create_feature_extractor(self.model_resnet, return_nodes=self.return_nodes)
 
         self.adaptive_avg_pool = torch.nn.AdaptiveAvgPool2d(1)
 
-        for layer_id in self.layers:
-            layer = dict([*self.model_resnet.named_modules()])[layer_id]
-            layer.register_forward_hook(self.save_outputs_hook(layer_id))
-
-    def save_outputs_hook(self, layer_id: str) -> Callable:
-        def fn(_, __, output):
-            self._features[layer_id] = output
-
-        return fn
-
     def forward(self, img, word_emb):
-        # low level feature extraction
-        # use the output feature maps from ResNet block 2 and 3 as low-level features.
-        # layer2.3.relu_2 -> torch.Size([1, 512, 28, 28])
-        # layer3.5.relu_2 -> torch.Size([1, 1024, 14, 14])
-        # Low feature = dict_keys(['layer2', 'layer3'])
-
-        # _ = self.model_resnet(img)
-
-        with torch.no_grad():
-            layer2 = self._features['layer2'].clone().to(self.device)
-            layer3 = self._features['layer3'].clone().to(self.device)
-
         img = self.model_resnet(img)
         word_emb = self.f_gate(word_emb)
 
-        # channel like product of fgate anf fimg
-        combination = torch.mul(img, word_emb)  # torch.Size([1, 2048])
+        # channel like product of fgate and fimg
+        composition = torch.mul(self.adaptive_avg_pool(img['layer4']).squeeze(3).squeeze(2), word_emb)  # torch.Size([1, 2048])
 
         # channel-wise concatenation of penultimate layer of Resnet and result of the compound module
-#        low_features = torch.cat((layer2, layer3.resize_(layer2.size())), dim=1).to(self.device)  # torch.Size([1, 1024, 28, 28])
+        layer2 = self.adaptive_avg_pool(img['layer2']).squeeze(3).squeeze(2)
+        layer3 = self.adaptive_avg_pool(img['layer3']).squeeze(3).squeeze(2)
         concatenation = torch.cat(
-            (self.adaptive_avg_pool(layer2).squeeze(3).squeeze(2),
-             self.adaptive_avg_pool(layer3).squeeze(3).squeeze(2)), dim=1
+            [layer2, layer3, composition], dim=1
         ).to(self.device) # torch.Size([1, 1024, 28, 28])
-#        concatenation = torch.cat((low_features.resize_(combination.size()), combination), dim=1).to(self.device)
-
-        # output = torch.sigmoid(self.classifier(concatenation))
+        
         output = self.classifier(concatenation)
 
         return output
@@ -81,7 +67,6 @@ if __name__ == "__main__":
 
     # shape is (batch, C, H, W)
     img = torch.rand(1, 3, 224, 224)
-    # img = torch.rand(1,3,512,812)
 
     # shape is (batch, dim)
     # 100d for pretrained GloVe embeddings
