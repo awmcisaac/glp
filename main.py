504b95fe0ac003acc1f4c625ca73a3e515b0c968
Andrew McIsaac <andrew.mcisaac@ntlworld.com>
2023-02-06 13:54:24 +0000

    Main training loop with config files for full and small dataset

---
+++ main.py
@@ -0,0 +1,129 @@
+from dataset import VAWDataset
+from trainer import Trainer
+from model import Model
+from glove import GloVeEmbedding
+from evaluator import Evaluator
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import argparse
+import json
+
+def main(args, config):
+    torch.manual_seed(42)
+    train_dataset = VAWDataset(
+        annotations_file=config["train_annotations"],
+        attrib_idx_file=config["attrib_idx_file"],
+        attrib_parent_types=config["attrib_parent_types"],
+        attrib_types=config["attrib_types"],
+        img_dir=config["img_dir"],
+        split="train",
+        resize=224
+    )
+    val_dataset = VAWDataset(
+        annotations_file=config["val_annotations"],
+        attrib_idx_file=config["attrib_idx_file"],
+        attrib_parent_types=config["attrib_parent_types"],
+        attrib_types=config["attrib_types"],
+        img_dir=config["img_dir"],
+        split="val",
+        resize=224
+    )
+    test_dataset = VAWDataset(
+        annotations_file=config["test_annotations"],
+        attrib_idx_file=config["attrib_idx_file"],
+        attrib_parent_types=config["attrib_parent_types"],
+        attrib_types=config["attrib_types"],
+        img_dir=config["img_dir"],
+        split="test",
+        resize=224
+    )
+
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=64,
+#        sampler=torch.utils.data.RandomSampler(train_dataset, num_samples=100),
+        shuffle=True,
+        num_workers=12,
+        pin_memory=True
+    )
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=64,
+#        sampler=torch.utils.data.RandomSampler(val_dataset, num_samples=100),
+        num_workers=12,
+        pin_memory=True,
+    )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=64,
+#        sampler=torch.utils.data.RandomSampler(train_dataset, num_samples=100),
+        num_workers=12,
+        pin_memory=True
+    )
+
+    loss_weights = torch.load(config["loss_weights"]).to(args.device)
+    loss_pos_weights = torch.load(config["loss_pos_weights"]).to(args.device)
+    loss_neg_weights = torch.load(config["loss_neg_weights"]).to(args.device)
+
+    model = Model(args.device)
+    loss = nn.BCEWithLogitsLoss(weight=loss_weights, pos_weight=loss_pos_weights)
+    #loss = nn.BCEWithLogitsLoss(pos_weight=loss_pos_weights)
+#    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5, weight_decay=1e-5)
+    optimizer = torch.optim.Adam([
+        {"params": model.model_resnet.parameters(), "lr": 1e-5},
+        {"params": model.f_gate.parameters()},
+        {"params": model.classifier.parameters()},
+        {"params": model.adaptive_avg_pool.parameters()}
+                 ], lr=7e-4, weight_decay=1e-5)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer,
+        mode="max",
+        factor=0.1,
+        patience=2
+    )
+    glove_embeddings = GloVeEmbedding(config["glove_embedding_file"])
+
+    evaluator = Evaluator(
+        fpath_attr2idx=config["attrib_idx_file"],
+        fpath_attr_type=config["attrib_types"],
+        fpath_attr_parent_type=config["attrib_parent_types"],
+        fpath_attr_headtail=config["attrib_head_tail"]
+    )
+
+    trainer = Trainer(
+        model, 
+        loss, 
+        optimizer, 
+        scheduler,
+        config,
+        glove_embeddings=glove_embeddings,
+        loss_weights=config["loss_weights"],
+        loss_pos_weights=config["loss_pos_weights"],
+        loss_neg_weights=config["loss_neg_weights"],
+        device=args.device,
+        attribute_index_path=config["attrib_idx_file"],
+        type_index_path=config["type_idx_file"],
+        attribute_types_full_path=config["attrib_types_full"]
+    )
+
+    trainer.fit(train_loader, val_loader, args.epochs)
+
+    trainer.test(test_loader)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_file", type=str, default="configs/base.json")
+
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    parser.add_argument("--device", type=str, default=device)
+    parser.add_argument("--epochs", type=int, default=50)
+    
+    args = parser.parse_args()
+    with open(args.config_file, "r") as f:
+        config = json.load(f)
+    main(args, config)
+
--- trainer.py
+++ trainer.py
@@ -5,6 +5,10 @@ import json
 import time
 import warnings
 import torch
+import torch.nn.functional as F
+import numpy as np
+from tqdm import tqdm
+from evaluator import Evaluator
 
 class Trainer:
     """Trainer
@@ -30,14 +34,26 @@ class Trainer:
         model, 
         criterion, 
         optimizer,
+        scheduler,
+        config,
+        glove_embeddings,
+        loss_weights,
+        loss_pos_weights,
+        loss_neg_weights,
         device=None,
         attribute_index_path: os.path = "data/attribute_index.json",
         type_index_path: os.path = "data/type_index.json",
         attribute_types_full_path: os.path = "data/attribute_types_full.json"
     ):
-        self.model = model
+        self.model = model.to(device)
         self.criterion = criterion
         self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.config = config
+        self.glove_embeddings = glove_embeddings
+        self.loss_weights = torch.load(loss_weights).to(device)
+        self.loss_pos_weights = torch.load(loss_pos_weights).to(device)
+        self.loss_neg_weights = torch.load(loss_neg_weights).to(device)
         self.device = self._get_device(device)
     
         with open(attribute_index_path) as f:
@@ -54,37 +70,54 @@ class Trainer:
         self.type2attrib = type2attrib
         
         # send model to device
-        self.model.to(self.device)
+        # self.model.to(self.device)
 
         # attributes        
         self.train_loss_ = []
         self.val_loss_ = []
         self.train_mean_acc_ = []
         self.val_mean_acc_ = []
+        self.val_map_ = []
         
     def fit(self, train_loader, val_loader, epochs):
         # track total training time
         total_start_time = time.time()
+
+        best_val_map = 0
 
         # ---- train process ----
         for epoch in range(epochs):
             # track epoch time
             epoch_start_time = time.time()
 
+            self.model.train(True)
             # train
             train_loss, train_mean_acc = self._train(train_loader)
-            
+
+            self.model.train(False)
             # validate
-            val_loss, val_mean_acc =self._validate(val_loader)
+            val_loss, val_mean_acc, val_map = self._validate(val_loader)
+            # apply LR decay of 0.1 when val mAP doesn't improve for 2 epochs
+            self.scheduler.step(val_map)
+
+            # save best
+            if val_map > best_val_map:
+                best_val_map = val_map
+                torch.save(self.model.state_dict(), f"models/model.pt")
+                print(f"New best model! Saved with mAP {best_val_map}")
             
-            print('[{:d}/{:d}]\t loss/train: {:.5f}\t loss/val: {:.5f}\t mA/train: {:.2f}\t mA/val: {:.2f}'.format(
-                epoch+1, epochs, train_loss, val_loss, train_mean_acc, val_mean_acc)
-                )
+            print('[{:d}/{:d}]\t loss/train: {:.5f}\t \
+                loss/val: {:.5f}\t mA/train: {:.2f}\t \
+                mA/val: {:.2f}\t mAP/val: {:.2f}'.format(
+                    epoch+1, epochs, train_loss, val_loss,
+                    train_mean_acc, val_mean_acc, val_map
+                ))
             
             self.train_loss_.append(train_loss)
             self.val_loss_.append(val_loss)
             self.train_mean_acc_.append(train_mean_acc)
             self.val_mean_acc_.append(val_mean_acc)
+            self.val_map_.append(val_map)
 
             epoch_time = time.time() - epoch_start_time
 
@@ -99,15 +132,20 @@ class Trainer:
         cumulative_acc_by_class = {i : 0. for i in range(8)}
         samples_by_class = {i : 0. for i in range(8)}
         
-        for inputs, targets in loader:
+        for batch in tqdm(loader):
             # move to device
-            inputs, targets = self._to_device(inputs, targets, self.device)
+            for k in batch.keys():
+                if isinstance(batch[k], torch.Tensor):
+                    batch[k] = batch[k].to(self.device)
+            inputs = batch["image"]
+            targets = batch["attributes_label"]
             
+            object_embeddings = self.glove_embeddings(batch["object_name"]).to(self.device)
             # forward pass
-            out = self.model(inputs)
+            out = self.model(batch["image"], object_embeddings)
             
             # loss
-            loss = self._compute_loss(out, targets)
+            loss = self._compute_loss(out, batch["attributes_label"])
             
             # remove gradient from previous passes
             self.optimizer.zero_grad()
@@ -119,18 +157,34 @@ class Trainer:
             self.optimizer.step()
 
             # n samples and cumulative accuracy by class 
-            for type in range(8):
-                predicted = torch.gather(input=out, dim=1, index=torch.LongTensor([self.type2attrib[type]]*inputs.shape[0])).round() # prediction round to 1 (positive) or 0 (negative, missing)
-                samples_by_class[type] += len(self.type2attrib[type])*inputs.shape[0]
-                cumulative_acc_by_class[type] += predicted.eq(torch.gather(input=targets, dim=1, index=torch.LongTensor([self.type2attrib[type]]*inputs.shape[0])).round()).sum().item()
+            for typ in range(8):
+                predicted = torch.gather(
+                    input=torch.sigmoid(out), 
+                    dim=1, 
+                    index=torch.LongTensor(
+                        [self.type2attrib[typ]]*inputs.shape[0]
+                    ).to(self.device)
+                ).round() # round to 1 (positive) or 0 (negative, missing)
+                samples_by_class[typ] += len(self.type2attrib[typ])*inputs.shape[0]
+                cumulative_acc_by_class[typ] += predicted.eq(
+                    torch.gather(
+                        input=targets, 
+                        dim=1, 
+                        index=torch.LongTensor(
+                            [self.type2attrib[typ]]*inputs.shape[0]
+                        ).to(self.device)
+                    ).round()
+                ).sum().item()
 
             # n samples and cumulative loss
             samples += inputs.shape[0]
             cumulative_loss += loss.item()
 
-            # train loss and train mean accuracy across classes
-            train_loss = cumulative_loss/samples
-            train_mean_accuracy = torch.mean(torch.tensor(list(cumulative_acc_by_class.values())) / torch.tensor(list(samples_by_class.values()))*100).item()
+        # train loss and train mean accuracy across classes
+        train_loss = cumulative_loss / samples
+        train_mean_accuracy = torch.mean(
+            torch.tensor(list(cumulative_acc_by_class.values())) \
+          / torch.tensor(list(samples_by_class.values()))*100).item()
 
         return train_loss, train_mean_accuracy
     
@@ -146,44 +200,199 @@ class Trainer:
         samples_by_class = {i : 0. for i in range(8)}
         
         with torch.no_grad():
-            for inputs, targets in loader:
+            full_predictions = []
+            full_targets = []
+            for batch in loader:
                 # move to device
-                inputs, targets = self._to_device(
-                    inputs, 
-                    targets, 
-                    self.device
-                )
-                
-                out = self.model(inputs)
+                for k in batch.keys():
+                    if isinstance(batch[k], torch.Tensor):
+                        batch[k] = batch[k].to(self.device)
+                inputs = batch["image"]
+                targets = batch["attributes_label"]
+
+                object_embeddings = self.glove_embeddings(batch["object_name"]).to(self.device)
+                out = self.model(inputs, object_embeddings)
+                sig_out = torch.sigmoid(out)
+                full_predictions.append(sig_out)
+                full_targets.append(targets)
                 loss = self._compute_loss(out, targets)
 
+
                 # n samples and cumulative accuracy by class
-                for type in range(8):
-                    predicted = torch.gather(input=out, dim=1, index=torch.LongTensor([self.type2attrib[type]]*inputs.shape[0])).round() # prediction round to 1 (positive) or 0 (negative, missing)
-                    samples_by_class[type] += len(self.type2attrib[type])*inputs.shape[0]
-                    cumulative_acc_by_class[type] += predicted.eq(torch.gather(input=targets, dim=1, index=torch.LongTensor([self.type2attrib[type]]*inputs.shape[0])).round()).sum().item()
+                for typ in range(8):
+                    predicted = torch.gather(
+                        input=sig_out, 
+                        dim=1, 
+                        index=torch.LongTensor(
+                            [self.type2attrib[typ]]*inputs.shape[0]
+                        ).to(self.device)
+                    ).round() # round to 1 (positive) or 0 (negative, missing)
+                    samples_by_class[typ] += len(self.type2attrib[typ])*inputs.shape[0]
+                    cumulative_acc_by_class[typ] += predicted.eq(
+                        torch.gather(
+                            input=targets, 
+                            dim=1, 
+                            index=torch.LongTensor(
+                                [self.type2attrib[typ]]*inputs.shape[0]
+                            ).to(self.device)
+                        ).round()
+                    ).sum().item()
 
                 # n samples and cumulative loss
                 samples += inputs.shape[0]
                 cumulative_loss += loss.item()
-                
-                # val loss and val mean accuracy across classes
-                val_loss = cumulative_loss/samples
-                val_mean_accuracy = torch.mean(torch.tensor(list(cumulative_acc_by_class.values())) / torch.tensor(list(samples_by_class.values()))*100).item()
 
-        return val_loss, val_mean_accuracy
+        # vaw_dataset evaluator
+        full_targets = torch.concat(full_targets, dim=0)
+        # TODO: change for soft-labels
+        full_targets = torch.where(full_targets==-1, 2, full_targets)
+        self.evaluator = Evaluator(
+            fpath_attr2idx=self.config["attrib_idx_file"],
+            fpath_attr_type=self.config["attrib_types"],
+            fpath_attr_parent_type=self.config["attrib_parent_types"],
+            fpath_attr_headtail=self.config["attrib_head_tail"]
+        )
+
+        scores_overall, scores_per_class = self.evaluator.evaluate(
+            pred=torch.concat(full_predictions, dim=0),
+            gt_label=full_targets,
+            threshold_type="topk"
+        )
+#        CATEGORIES = ['all', 'head', 'medium', 'tail'] + \
+#            list(self.evaluator.attribute_parent_type.keys())
+#
+#        for category in CATEGORIES:
+#            print(f"----------{category.upper()}----------")
+#            print(f"mAP: {scores_per_class[category]['ap']:.4f}")
+#
+#            print("Per-class (top 15):")
+#            for metric in ['recall', 'precision', 'f1', 'bacc']:
+#                if metric in scores_per_class[category]:
+#                    print(f"- {metric}: {scores_per_class[category][metric]:.4f}")
+#
+#            print("Overall (top 15):")
+#            for metric in ['recall', 'precision', 'f1']:
+#                if metric in scores_overall[category]:
+#                    print(f"- {metric}: {scores_overall[category][metric]:.4f}")
+#        with open('detailed_output.txt', 'w') as f:
+#            f.write('| {:<18}| AP\t\t| Recall@K\t| B.Accuracy\t| N_Pos\t| N_Neg\t|\n'.format('Name'))
+#            f.write('-----------------------------------------------------------------------------------------------------\n')
+#            for i_class in range(self.evaluator.n_class):
+#                att = self.evaluator.idx2attr[i_class]
+#                f.write('| {:<18}| {:.4f}\t| {:.4f}\t| {:.4f}\t\t| {:<6}| {:<6}|\n'.format(
+#                    att,
+#                    self.evaluator.get_score_class(i_class).ap,
+#                    self.evaluator.get_score_class(i_class, threshold_type='topk').get_recall(),
+#                    self.evaluator.get_score_class(i_class).get_bacc(),
+#                    self.evaluator.get_score_class(i_class).n_pos,
+#                    self.evaluator.get_score_class(i_class).n_neg))
+#        ap_list = []
+#        for i_class in range(self.evaluator.n_class):
+#            att = self.evaluator.idx2attr[i_class]
+#            ap_list.append(self.evaluator.get_score_class(i_class).ap)
+#        val_map = sum(ap_list) / len(ap_list))
+        val_map = np.mean(
+            [att.ap for att in 
+             map(self.evaluator.get_score_class, range(self.evaluator.n_class))
+            ])
+
+#        print(scores_overall)
+#        print(scores_per_class)
+
+        # val loss and val mean accuracy across classes
+        val_loss = cumulative_loss / samples
+        val_mean_accuracy = torch.mean(torch.tensor(list(cumulative_acc_by_class.values())) / torch.tensor(list(samples_by_class.values()))*100).item()
+
+        return val_loss, val_mean_accuracy, val_map
+
+    def test(self, test_loader):
+        print("Running test inference with best validation model")
+        self.model.load_state_dict(torch.load('models/model.pt'))
+        self.model.eval()
+
+        with torch.no_grad():
+            full_predictions = []
+            full_targets = []
+            for batch in loader:
+                # move to device
+                for k in batch.keys():
+                    if isinstance(batch[k], torch.Tensor):
+                        batch[k] = batch[k].to(self.device)
+                inputs = batch["image"]
+                targets = batch["attributes_label"]
+
+                object_embeddings = self.glove_embeddings(batch["object_name"]).to(self.device)
+                out = self.model(inputs, object_embeddings)
+                sig_out = torch.sigmoid(out)
+                full_predictions.append(sig_out)
+                full_targets.append(targets)
+                loss = self._compute_loss(out, targets)
+
+        # vaw_dataset evaluator
+        full_targets = torch.concat(full_targets, dim=0)
+        # TODO: change for soft-labels
+        full_targets = torch.where(full_targets==-1, 2, full_targets)
+        self.evaluator = Evaluator(
+            fpath_attr2idx=self.config["attrib_idx_file"],
+            fpath_attr_type=self.config["attrib_types"],
+            fpath_attr_parent_type=self.config["attrib_parent_types"],
+            fpath_attr_headtail=self.config["attrib_head_tail"]
+        )
+
+        scores_overall, scores_per_class = self.evaluator.evaluate(
+            pred=torch.concat(full_predictions, dim=0),
+            gt_label=full_targets,
+            threshold_type="topk"
+        )
+        test_map = np.mean(
+            [att.ap for att in 
+             map(self.evaluator.get_score_class, range(self.evaluator.n_class))
+            ])
+
+        np.save(f"eval/test_preds_{test_map}.npy", torch.concat(full_predictions, dim=0).cpu().numpy())
+        np.save(f"eval/test_gt_{test_map}.npy", full_targets.cpu().numpy())
+        print(f"Saved test predictions and GTs to eval/test_(preds/gt)_{test_map}.npy")
+
+        return test_map
+
+#    def _compute_metrics(self, cache, classes, topk=15):
+#        sum_vals = {k: sum(v) for k, v in cache.items()}
+#        gts = torch.cat(cache["gt"], dim=0)
+#        preds = torch.cat(cache["preds"], dim=0)
+#        mAP = self.calculate_mAP(preds, gts)
+#        PC_Recall = sum(
+#            sum_vals[f"TP_{cls}"] / max(sum_vals[f"P_{cls"], 1e-5) for cls in classes
+#        ) / len(classes)
+#        PC_Precision = sum(
+#            sum_vals[f"TP_{cls}"] / max(sum_vals[f"PP_{cls"], 1e-5) for cls in classes
+#        ) / len(classes)
+#        OV_Recall = sum(sum_vals[f"TP_{cls}"] for cls in classes) / sum(
+#            sum_vals[f"P_{cls}"] for cls in classes
+#        )
+#        OV_Precision = sum(sum_vals[f"TP_{cls}"] for cls in classes) / sum(
+#            sum_vals[f"PP_{cls}"] for cls in classes
+#        )
+#        OV_F1 = 2 * OV_Recall * OV_Precision / max(OV_Recall + OV_Precision, 1e-5)
+#        PC_F1 = 2 * PC_Recall * PC_Precision / max(PC_Recall + PC_Precision, 1e-5)
     
     def _compute_loss(self, real, target):
-        try:
-            loss = self.criterion(real, target)
-        except:
-            loss = self.criterion(real, target.long())
-            msg = f"Target tensor has been casted from"
-            msg = f"{msg} {type(target)} to 'long' dtype to avoid errors."
-            warnings.warn(msg)
+        # get indices of unlabeled attributes
+        weights = torch.where(target==-1., 0.05*self.loss_weights, 1.*self.loss_weights)
+        # weights = torch.where(target==-1., 0.05, 1.)
+        # TODO: change for soft-labels
+        target = torch.where(target==-1., 0., target)
+#        loss = F.binary_cross_entropy_with_logits(real, target, weight=weights)
 
-        # apply regularization if any
-        # loss += penalty.item()
+        # have to do this manually to also include negative weights
+        max_val = (-real).clamp_min(0)
+        log_pos_weight = self.loss_pos_weights.mul(target)
+        log_neg_weight = self.loss_neg_weights.mul(1 - target)
+        log_weight = log_pos_weight + log_neg_weight
+        # this kind of works. not sure about stability though
+        loss = (1 - target).mul(real).add(((-real).exp().add(1)).log()).mul(log_weight)
+
+        loss = loss * self.loss_weights
+        loss = loss.mean()
             
         return loss
 
